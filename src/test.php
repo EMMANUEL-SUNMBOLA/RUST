6cd7d374fc367cc757f7e5e8f474425a302dca45
Emmanuel-Sunmbola <adedayoemmanuel729@gmail.com>
2025-08-30 04:33:38 +0100

    random function

--- src/test.php
+++ src/test.php
@@ -1,22 +1,39 @@
 <?php
 
-$secret = rand(1, 100);
-echo $secret . "\n";
-$guess = readline("Input your guess ? \n");
 
-while($guess !== $secret){
+function fibb($n){
 
-    
-    if ($guess > $secret){
-        echo "go lower \n";
-    }elseif($guess < $secret){
-        echo "go higher \n";
-    }elseif($guess == $secret){
-        echo "Hurray you got it 😀 \n";
-        break;
-    }
-    
-    $guess = readline("Try again ? \n");
+    if($n <= 0) return 0;
+    if($n === 1) return 1;
+
+    return (fibb($n - 1) + fibb($n - 2));
+
 }
+
+
+$int = readline("what nth fibb do you want   ");
+
+for($i = 0; $i <= $int; $i++){
+    echo $i . " - " . fibb($i) . "\n";
+}
+
+// $secret = rand(1, 100);
+// echo $secret . "\n";
+// $guess = readline("Input your guess ? \n");
+
+// while($guess !== $secret){
+
+    
+//     if ($guess > $secret){
+//         echo "go lower \n";
+//     }elseif($guess < $secret){
+//         echo "go higher \n";
+//     }elseif($guess == $secret){
+//         echo "Hurray you got it 😀 \n";
+//         break;
+//     }
+    
+//     $guess = readline("Try again ? \n");
+// }
 
 // Future Improvements, add a clue button that uses the numbers API to fetch some details about the random number !!!!
